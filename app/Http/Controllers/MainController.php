9161ce676ce3f3a24475d0c5864484f6a945063f
oong <mkhalil26122000@gmail.com>
2021-02-08 22:54:40 +0700

    first commit

---
+++ app/Http/Controllers/MainController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Penjualan;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class MainController extends Controller
+{
+    public function getTotalByDate($date)
+    {
+        $penjualan = Penjualan::select(
+                                    DB::raw('SUM(penjualan.total_harga - penjualan.total_diskon) AS total'),
+                                    DB::raw('SUM(penjualan.total_ppn) AS total_ppn')
+                                )
+                                ->whereBetween('penjualan.waktu', [$date.' 00:00:00', $date.' 23:59:59'])  
+                                ->where('penjualan.status_bayar', 'Sudah Bayar')
+                                ->get();
+
+        return json_encode($penjualan);
+    }
+}
